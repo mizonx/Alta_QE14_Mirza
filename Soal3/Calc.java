10d074d2b77571e8f2cb03b813331b87727da39f
mizonx <67965366+mizonx@users.noreply.github.com>
2024-03-08 17:05:32 +0700

    Soal 3

---
+++ Soal3/Calc.java
@@ -0,0 +1,16 @@
+package OOP.Soal3;
+
+public class Calc {
+    public static int penjumlahan(int x, int y){
+        return x+y;
+    }
+    public static int pengurangan(int x, int y){
+        return x-y;
+    }
+    public static int perkalian(int x, int y){
+        return x*y;
+    }
+    public static int pembagian(int x, int y){
+        return x/y;
+    }
+}
---
+++ Soal3/Main.java
@@ -0,0 +1,10 @@
+package OOP.Soal3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Penjumlahan(3,4) = "+Calc.penjumlahan(3,4));
+        System.out.println("Pengurangan(15,4) = "+Calc.pengurangan(15,4));
+        System.out.println("Perkalian(10,10) = "+Calc.perkalian(10,10));
+        System.out.println("Pembagian(12,3) = "+Calc.pembagian(12,3));
+    }
+}
